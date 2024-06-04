f92957c03bb1ab5cd28a4d7c0c75776899422ea9
Ghlais <117529494+Ghlais@users.noreply.github.com>
2024-06-04 15:02:09 +0300

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,1 +1,41 @@
-.
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+const { MessageActionRow, MessageButton, MessageEmbed } = require('discord.js');
+
+module.exports = {
+
+  data: new SlashCommandBuilder()
+
+    .setName('embed')
+
+    .setDescription('Sends an embed with a button'),
+
+  async execute(interaction) {
+
+    const embed = new MessageEmbed()
+
+      .setColor('#0099ff')
+
+      .setTitle('Example Embed')
+
+      .setDescription('This is an example embed with a button.');
+
+    const row = new MessageActionRow()
+
+      .addComponents(
+
+        new MessageButton()
+
+          .setCustomId('primary')
+
+          .setLabel('Press me')
+
+          .setStyle('PRIMARY'),
+
+      );
+
+    await interaction.reply({ embeds: [embed], components: [row] });
+
+  },
+
+};
