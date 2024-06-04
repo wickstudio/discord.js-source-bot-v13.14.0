e0f70a6380ca638c3cc0cdd94fb9f1b744447fe8
Ghlais <117529494+Ghlais@users.noreply.github.com>
2024-06-04 15:05:03 +0300

    Create interactionCreate.js

---
+++ events/interactionCreate.js
@@ -0,0 +1,22 @@
+module.exports = {
+  name: 'interactionCreate',
+  async execute(interaction) {
+    if (interaction.isCommand()) {
+      const command = interaction.client.commands.get(interaction.commandName);
+      if (!command) return;
+      try {
+       await command.execute(interaction);
+      } catch (error) {
+        console.error(error);
+        await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+      }
+    } else if (interaction.isButton()) {
+      if (interaction.customId === 'primary') {
+        await interaction.reply({ content: 'There was!', ephemeral: true });
+      }
+
+    }
+
+  },
+
+};
