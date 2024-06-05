7d4c73377c2f53c9726fc603a7ac929def6ff414
Wick Studio <53820568+wickstudio@users.noreply.github.com>
2024-06-05 16:25:57 +0300

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -2,14 +2,11 @@
 :start
 echo 1 - Install Dependencies
 echo 2 - Start Bot
-echo 3 - Run The Bot
 set /p choice=Enter a number: 
 if %choice%==1 (
     npm install
 ) else if %choice%==2 (
     node index.js
-) else if %choice%==3 (
-    npm start
 ) else (
     echo Invalid choice
     cls
